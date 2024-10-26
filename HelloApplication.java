08afe85a6556305736e325860f9f59ba2694c3ae
Artemalatoo <150508652+Artemalatoo@users.noreply.github.com>
2024-10-26 12:35:43 +0600

    Create HelloApplication.java

---
+++ HelloApplication.java
@@ -0,0 +1,23 @@
+package org.example.bmi;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Ensure that "bmi-view.fxml" is placed in "src/org/example/bmi" or resources folder
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("bmi-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        primaryStage.setTitle("BMI Calculator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
